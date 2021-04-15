dd131a05f472307df27065ba7801500ecf6805e1
Mathitis <elouisa@student.ysu.edu>
2021-04-15 17:00:35 -0400

    Added sliders & label for each color val. Implemented gridpane

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,10 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -17,9 +21,27 @@ public class Main extends Application {
     }
 
 
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
 
         Button lightSwitch = new Button("On/Off");
+
+        Label redVal = new Label("red Val");
+        Label blueVal = new Label("blue Val");
+        Label greenVal =  new Label("green Val");
+
+        Slider redSlider   = new Slider(0,255,0);
+        redSlider.setShowTickLabels(true);
+        redSlider.setLayoutY(10);
+
+        Slider blueSlider  = new Slider(0, 255, 0);
+        blueSlider.setShowTickLabels(true);
+        blueSlider.setLayoutY(40);
+        blueSlider.setShowTickLabels(true);
+
+        Slider greenSlider = new Slider(0,255,0);
+        greenSlider.setShowTickLabels(true);
+        greenSlider.setLayoutY(70);
+        greenSlider.setShowTickLabels(true);
 
         lightSwitch.setLayoutX(250);
         lightSwitch.setLayoutY(250);
@@ -35,14 +57,31 @@ public class Main extends Application {
             }
         });
 
-        Group root = new Group(lightSwitch);
-        Scene scene = new Scene(root,600, 300);
+//        Group root = new Group();
+//
+//        root.getChildren().addAll(redSlider, blueSlider, greenSlider, lightSwitch);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setMinSize(400, 200);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        gridPane.add(redSlider, 0,0);
+        gridPane.add(blueSlider, 0 , 1);
+        gridPane.add(greenSlider, 0 ,2);
+        gridPane.add(redVal, 1,0);
+        gridPane.add(blueVal, 1,1);
+        gridPane.add(greenVal,1, 2);
+        gridPane.add(lightSwitch, 1,3);
+
+        Scene scene = new Scene(gridPane,600, 300);
         scene.setFill(Color.SNOW);
 
-        primaryStage.setTitle("Arduino RGB Controller");
-        primaryStage.setAlwaysOnTop(true);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("Arduino RGB Controller");
+        stage.setScene(scene);
+        stage.show();
 
 
     }
